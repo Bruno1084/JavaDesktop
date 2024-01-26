515448239b94801bbb3647c1b2c382c03b06f1da
Bruno Sosa <sosabruno3384@gmail.com>
2024-01-26 19:54:48 -0300

    Variables added to InicioController

--- src/main/java/com/javafxtest/InicioController.java
+++ src/main/java/com/javafxtest/InicioController.java
@@ -1,6 +1,30 @@
 package com.javafxtest;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class InicioController {
+
+    @FXML
+    private TextField searchBar;
+    @FXML
+    private MenuButton filterButton;
+    @FXML
+    private Text daySellingsText;
+    @FXML
+    private Text monthSellingsText;
+    @FXML
+    private LineChart graphChart;
+    @FXML
+    private TableView tableVentas;
+
+
+
 
 
 }
